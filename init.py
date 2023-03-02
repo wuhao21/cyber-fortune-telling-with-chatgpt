2dfad68015946b39705eba2c6e8c9e3f3ef80c3c
wuhao21 <wu.hao.cz.21@gmail.com>
2023-03-02 00:25:00 -0800

    plugin chatgpt API

--- init.py
+++ init.py
@@ -1,49 +1,38 @@
 import json
 import os
+import random
+import openai
+from dotenv import load_dotenv
 from typing import Dict, List
-
-
-class TarotCard:
-    def __init__(
-        self,
-        name: str,
-        rank: int,
-        suite: str,
-        meanings: Dict[str, List[str]],
-        keywords: List[str],
-        fortune_telling: List[str],
-    ):
-        self.name = name
-        self.rank = rank
-        self.suite = suite
-        self.meanings = meanings
-        self.keywords = keywords
-        self.fortune_telling = fortune_telling
-
-    @classmethod
-    def from_json(cls, json_data):
-        return cls(
-            json_data["name"],
-            json_data["rank"],
-            json_data["suit"],
-            json_data["meanings"],
-            json_data["keywords"],
-            json_data["fortune_telling"],
-        )
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-
 
 # Get the path to the Tarot deck JSON file in the user's home directory
 filepath = os.path.expanduser("./data/tarot_interpretations.json")
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 # Open the Tarot deck JSON file
 with open(filepath, "r") as file:
     data = json.load(file)
 # Create a list of TarotCard objects for each card in the deck
 tarot_cards = []
 for card_json in data["tarot_interpretations"]:
-    tarot_cards.append(TarotCard.from_json(card_json))
+    tarot_cards.append(card_json["name"])
 
-print(len(tarot_cards))
-print(tarot_cards[21].toJSON())
+drew_cards = ", ".join(random.choices(tarot_cards, k=3))[:-2]
+systemSetting_EN = "You are a fortune teller and good at Tarot."
+promot_EN = "I drew 3 cards: "+drew_cards + \
+    ". Please tell me the meaning of 3 cards and my fortune today in JSON format {\"cards\":[{\"name\": string, \"meaning\": string}],\"fortune\": string)}:"
+systemSetting_CN = "你是一个精通塔罗牌的算命大师"
+promot_CN = "我抽了三张牌: "+drew_cards + \
+    ". 请用中文回答我这三张牌的含义以及我今日的运势 把结果用一下JSON的形式告诉我 {\"cards\":[{\"name\": string, \"meaning\": string}],\"fortune\": string)}"
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    temperature=0,
+    messages=[
+        {"role": "system", "content": systemSetting_CN},
+        {"role": "user", "content": promot_CN},
+    ]
+)
+responseContent = response["choices"][0]["message"]["content"]
+print(response)
+result = json.loads(responseContent.replace('，',',')) 
+print(result)
